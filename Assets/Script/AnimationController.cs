b7032d8b4a9117c38f5b7bd50b2fec036dd4f243
Mike Notebook <Mick60316@gmail.com>
2021-01-13 20:30:10 +0800

    Work from home animation

---
+++ Assets/Script/AnimationController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+public class AnimationController : MonoBehaviour
+{
+
+    public Animator m_PresentationAnimator;
+    public Animator m_FourPeopleAnimator;
+    public Animator m_TwoPeopleAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_PresentationAnimator.Play("PowerPointSelect");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            m_PresentationAnimator.Play("Normal");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_FourPeopleAnimator.Play("ZoomIn");
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            m_FourPeopleAnimator.Play("ZoomOut");
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            m_FourPeopleAnimator.Play("MoveAndFadeOut");
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            m_TwoPeopleAnimator.Play("Normal");
+
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            m_TwoPeopleAnimator.Play("Create");
+
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            m_TwoPeopleAnimator.Play("Join");
+
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            m_TwoPeopleAnimator.Play("FullScreen");
+
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            m_TwoPeopleAnimator.Play("ShareScreen");
+
+        }
+
+
+
+
+    }
+    
+
+
+}
---
+++ Assets/Script/MyVideoPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+public class MyVideoPlayer : MonoBehaviour
+{
+
+    VideoPlayer m_VideoPlayer;
+    RawImage m_RawImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_VideoPlayer = GetComponent<VideoPlayer>();
+        m_RawImage = GetComponent<RawImage>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_RawImage.texture = m_VideoPlayer.texture;
+    }
+}
